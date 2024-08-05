d0e9bb379efade1cebf80738e73570a25522c8c6
erick <erickgtl2001@gmail.com>
2024-08-05 22:21:21 +0800

    feat: authentication schemas

---
+++ schemas/user_schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
+from enum import Enum
+import datetime
+
+class RegisterUser(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    mobile: str
+    registered_at: datetime.datetime
+    profile: str
+    disabled: bool = Field(default=False)
+
+class UserOutput(BaseModel):
+    user_id: int
+    username: str
+    email: EmailStr
+    mobile: str
+    registered_at: datetime.datetime
+    profile: str
+    disabled: bool
+
+class UpdateUser(BaseModel):
+    user_id: int
+    username: str
+    email: EmailStr
+    mobile: str
+    profile: str
+
+
